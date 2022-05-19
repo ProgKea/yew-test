b6dfcc160b7c117d88c1042405001decc43319d3
ProgKea <hoschy01@googlemail.com>
2022-05-19 16:41:02 +0200

    learned about properties

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,23 @@ use gloo::console::log;
 use stylist::{style, yew::styled_component};
 use yew::prelude::*;
 
+mod components;
+
+use components::atoms::main_title::MainTitle;
+
 #[styled_component(App)]
 pub fn app() -> Html {
     let my_style = style!(
         r#"
         color: gold;
         font-family: monospace;
+        font-size: 64px;
         "#
     ).unwrap();
 
     html! {
-        <>
-            <h1 class={my_style}>{"Gorillatype"}</h1>
-        </>
+        <div class={my_style}>
+            <MainTitle title="Test"/>
+        </div>
     }
 }
