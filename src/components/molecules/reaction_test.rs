3bdd9cc34d9b615312bfd1d2ace5869b15a7cd27
ProgKea <hoschy01@googlemail.com>
2022-05-20 14:24:18 +0200

    added reaction test file

---
+++ src/components/molecules/reaction_test.rs
@@ -0,0 +1,10 @@
+#[allow(unused)]
+
+use yew::prelude::*;
+use stylist::{yew::styled_component, style};
+use crate::components::atoms::colored_box::{Color, ColoredBox};
+
+#[styled_component(ReactionTest)]
+pub fn reaction_test() -> Html {
+
+}
